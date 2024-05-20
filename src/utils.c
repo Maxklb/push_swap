d9fa4a4442e18c208b07d87308e7260e8b67adb2
Maxklb <themaxklb@gmail.com>
2024-05-20 17:17:39 +0200

    utils

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:35:43 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/05/10 17:49:25 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/05/20 17:14:47 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,34 @@ void	ft_putstr(char *str)
 		write(1, &str[i], 1);
 		i++;
 	}
+}
+
+int	nb_absolute(int nb)
+{
+	if (nb < 0)
+		return (-nb);
+	return (nb);
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack *tmp;
+
+	if(*stack == NULL || stack == NULL)
+		return ;
+	while (*stack)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		free(tmp);
+	}
+	*stack = NULL;
+}
+
+void	exit_error(t_stack **stack_a, t_stack **stack_b)
+{
+	free_stack(stack_a);
+	free_stack(stack_b);
+	ft_putstr("Error\n");
+	exit(1);
 }
